6dc3c6d184473fe78ee5a684af39ca2d30ec8c8b
Cornelia Kelinske <corneliakelinske@gmail.com>
2021-06-10 07:10:14 -0700

    combined all the controllre tests in one test file

--- lib/first_blog_web/controllers/blog.controller.ex
+++
@@ -1,7 +0,0 @@
-# defmodule FirstBlogWeb.BlogController do
-#   use FirstBlogWeb, :controller
-
-#   def blog(conn, _oarams) do
-#     render(conn, "blog.html")
-#   end
-# end
--- test/first_blog_web/controllers/about_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule FirstBlogWeb.PageControllerTest do
-  use FirstBlogWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/about")
-    assert html_response(conn, 200) =~ "All About ME!"
-  end
-end
