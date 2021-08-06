cc146042c5a244e1013dacac54e708bb46717720
Cornelia Kelinske <corneliakelinske@gmail.com>
2021-08-06 12:41:19 -0700

    started on pagination; I get buttons, but there is a problem once I am creating more than the set number of posts per page

--- test/first_blog_web/controllers/post_controller_test.exs
+++ test/first_blog_web/controllers/post_controller_test.exs
@@ -5,7 +5,6 @@ defmodule FirstBlogWeb.PostControllerTest do
     test "with valid post ID renders the post", %{conn: conn} do
       id = "hello_world"
 
-
       conn = get(conn, Routes.post_path(conn, :show, id))
       assert html_response(conn, 200) =~ "Where to start?"
     end
