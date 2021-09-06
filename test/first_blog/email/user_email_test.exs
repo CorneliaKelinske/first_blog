eec17c03799648812e6cc9b4eda5d7279598b56e
Cornelia Kelinske <corneliakelinske@gmail.com>
2021-09-06 10:08:55 -0700

    getting started on the user_email test

---
+++ test/first_blog/email/user_email_test.exs
@@ -0,0 +1,15 @@
+defmodule FirstBlog.UserEmailTest do
+  use FirstBlog.DataCase
+  alias FirstBlog.UserEmail
+
+  #@valid_data %{from_email => "some_email", name => "rando", subject => "random", message => "blablab"}
+
+  test "always pass" do
+    assert true
+  end
+
+
+
+  
+
+end
