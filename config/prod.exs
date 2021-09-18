21374267b4b55f9e3aa84f46e0718812ade40c0a
Cornelia Kelinske <corneliakelinske@gmail.com>
2021-09-18 14:25:43 -0700

    files for deploy added
    Co-authored-by: Alan Vardy <alan@alanvardy.com>

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,9 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :first_blog, FirstBlogWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+url: [host: System.get_env("WEB_HOST"), port: 5000],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  load_from_system_env: true,
 
 # Do not print debug messages in production
 config :logger, level: :info
